adfd31807136b96cbfb4780abe9f4199b7e43f8d
thejovamsi <thejovamsi9581300609@gmail.com>
2025-08-20 18:35:48 -0400

    filling the form

--- server/routes/Puppeteer.js
+++ server/routes/Puppeteer.js
@@ -30,14 +30,73 @@ const selectedTime = timefilter[datePosted];
 
 // await new Promise(resolve => setTimeout(resolve, 2000));
 
-await page.goto(`https://www.linkedin.com/jobs/search-results/?distance=25&f_AL=true&f_TPR=r${selectedTime}&geoId=1034644278&keywords=${encodeURIComponent(jobTitle)}&origin=SEMANTIC_SEARCH_HISTORY` , {waitUntil: 'networkidle2'});
+await new Promise(resolve => setTimeout(resolve, 5000));
+
+
+await page.goto(`https://www.linkedin.com/jobs/search-results/?distance=25&f_AL=true&f_TPR=r${selectedTime}&geoId=1034644278&keywords=${encodeURIComponent(jobDescription)}&origin=SEMANTIC_SEARCH_HISTORY`, { waitUntil: 'networkidle2' });
 await new Promise(resolve => setTimeout(resolve, 2000));
 
+await page.locator('.semantic-search-results-list').wait();
+
+await page.locator('.semantic-search-results-list').scroll({ scrollTop: 2000, behavior: 'smooth'});
+await new Promise(resolve => setTimeout(resolve, 2000));
+
+// Click the first LinkedIn job card using the class name and anchor tag
+await page.locator('a.job-card-job-posting-card-wrapper__card-link').wait();
+const jobCards = await page.$$('a.job-card-job-posting-card-wrapper__card-link');
+if (jobCards.length > 0) {
+  await jobCards[0].click();
+}
+console.log("Clicked the first job card.", jobCards.length);
+
+await page.locator('#jobs-apply-button-id').wait();
+await page.locator('#jobs-apply-button-id').click();
+console.log("Clicked the Easy Apply button.");
+
+await page.waitForSelector('form', { visible: true });
+
+//entering the email in the form which is in select box with options value
+await page.waitForSelector('select[id*="multipleChoice"]', { visible: true });
+
+// choose one:
+const EMAIL = 'thejovamsigajjala@gmail.com';
+// const EMAIL = 'thejovamsi9581300609@gmail.com';
+
+await page.select('select[id*="multipleChoice"]', EMAIL);
+console.log("Selected the email field.");
+
+//
+
+const phoneNumberValue = await page.waitForSelector('input[id*="phoneNumber"]', { visible: true })
+if (phoneNumberValue === '') {
+    console.log("Phone number field is empty, proceeding to type the number.");
+    await page.type('input[id*="phoneNumber"]', '2014238100');
+} else {
+    console.log("Phone number field is already filled, skipping this step.");
+
+}
+// await page.waitForSelector('input[id*="phoneNumber"]', { visible: true });
+// await page.type('input[id*="phoneNumber"]', '2014238100');
+
+await new Promise(resolve => setTimeout(resolve, 3000));
+await page.locator('button[aria-label="Continue to next step"]').click();
+await new Promise(resolve => setTimeout(resolve, 3000));
 
 
+await page.locator('button[aria-label="Continue to next step"]').click();
+// await new Promise(resolve => setTimeout(resolve, 2000));
+
+// await page.waitForSelector('select.fb-dash-form-element__select-dropdown', { visible: true });
+// await page.select('select.fb-dash-form-element__select-dropdown', 'Afghanistan (+93)');
+// console.log("Selected the phone code field.");
+// await new Promise(resolve => setTimeout(resolve, 2000));
+
+
+
+// await new Promise(resolve => setTimeout(resolve, 2000));
 
 // console.log("Logged in to LinkedIn");
-// search for jobs
+// search for job`s
 
 // await page.goto('https://www.linkedin.com/jobs', {waitUntil: 'networkidle2'});
 
@@ -70,9 +129,6 @@ await new Promise(resolve => setTimeout(resolve, 2000));
 // await page.click('a[href*="/jobs/view/"]:first-child');
 
 // await page.locator('jobs-apply-button--top-card').click();
-
-
-await new Promise(resolve => setTimeout(resolve, 10000));
 
 // selecting the first job from the results
 // await page.waitForSelector('job-card-job-posting-card-wrapper')
